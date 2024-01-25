4c8a1a1d4e39cbc785a7c3660a79a0a41260d692
choetaeyang <imsun0101@naver.com>
2024-01-25 11:26:12 +0900

    Initial project setup completed

---
+++ src/main/java/com/stocktalkhub/stocktalkhub/StocktalkhubApplication.java
@@ -0,0 +1,13 @@
+package com.stocktalkhub.stocktalkhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktalkhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktalkhubApplication.class, args);
+	}
+
+}
